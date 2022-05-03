f022bba010a34262a8ad2d582e1bc29dea30f30e
Matthew Sentell <msentell7@gmail.com>
2022-05-03 15:35:31 -0400

    order hist prep

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -18,6 +18,13 @@ namespace CineWeb.Controllers
         public OrderController(WebContext context) {
             _context = context;
         }
+        // start new order
+        public async Task<IActionResult> Index() {
+            return View();
+        }
         // get order history by user
+        public async Task<IActionResult> History() {
+
+        }
     }
 }
